5be810426d980bf44c299cd427a3a932c95ad526
SkylineHR31 <skyline_hr_31@outlook.com>
2021-10-28 04:10:10 +0300

    finished desktop version

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // dependencies layer
 import 'normalize.css';
 import "../../node_modules/@fortawesome/fontawesome-free/css/all.css";
+
 // developers styles layer
 import "../css/global.scss";
 import "../css/layout.scss";
@@ -10,3 +11,7 @@ import "../css/mixins.scss";
 import "../css/sections/helpingSection.scss";
 import "../css/sections/whySection.scss";
 import "../css/sections/aboutSection.scss";
+import "../css/sections/canHelpSection.scss";
+import "../css/sections/gallerySection.scss";
+import "../css/sections/effectsSection.scss";
+import "../css/sections/knowSection.scss";
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "/src/js/index.js",
@@ -35,6 +35,10 @@ module.exports = {
             },
           },
         ],
+      },
+      {
+        test: /\.(html)$/,
+        use: ["html-loader"],
       },
     ],
   },
